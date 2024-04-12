ff93022b0a631a23ee6266b92efd5ba3f8e6c47e
Thomas Steiner <tomac@google.com>
2024-04-12 11:00:06 +0200

    Add files via upload

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "**/*.{html,js,css}",
+    "videos/*.mp4"  // Assuming videos are in a 'videos' directory
+  ],
+  "swDest": "sw.js"
+};
